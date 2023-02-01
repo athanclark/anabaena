2ec3b34fd9386f29591a7679a10c56d24c12826f
Athan Clark <athan.clark@gmail.com>
2023-02-01 12:33:45 -0500

    trying to run example

--- shell.nix
+++ shell.nix
@@ -1,4 +1,23 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    nativeBuildInputs = [ pkgs.gcc pkgs.pkg-config ];
+    nativeBuildInputs = [
+      pkgs.gcc
+      pkgs.pkg-config
+      pkgs.xorg.libX11
+      pkgs.xorg.libX11.dev
+      pkgs.xorg.libXcursor
+      pkgs.xorg.libXcursor.dev
+      pkgs.xorg.libXrandr
+      pkgs.xorg.libXrandr.dev
+      pkgs.xorg.libXi
+      pkgs.xorg.libXi.dev
+      pkgs.libGL
+      pkgs.libGL.dev
+      pkgs.libGLU
+      pkgs.libGLU.dev
+      pkgs.glew-egl
+      pkgs.glew-egl.dev
+      pkgs.libglvnd
+      pkgs.libglvnd.dev
+    ];
   }
